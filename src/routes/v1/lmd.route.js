bc5b5a9fc517be66506879913a1eb4f1cf0c7b0f
Dhruvin.rajani <backenddeveloper@chetascontrol.com>
2024-03-18 14:48:43 +0530

    2 api auth token add

--- src/routes/v1/lmd.route.js
+++ src/routes/v1/lmd.route.js
@@ -10,8 +10,8 @@ router.get('/get-salientfeature', auth(), lmdController.getSalientFeature);
 router.get('/lmd-overview', auth(), lmdController.lmdDamOverview);
 router.get('/overview-advm', auth(), lmdController.getLastDataLmdDamSpareAdvm);
 
-router.get('/lmd-1to20-dis-gatereport', lmdController.lmdDischargeGateReport);
-router.get('/lmd-1to20-opn-gatereport', lmdController.lmdOpeningGateReport);
+router.get('/lmd-1to20-dis-gatereport', auth(), lmdController.lmdDischargeGateReport);
+router.get('/lmd-1to20-opn-gatereport', auth(), lmdController.lmdOpeningGateReport);
 router.get('/lmd-pondlevel-report',lmdController.lmdPondlevelGateReport);
 router.get('/lmd-parameter-overview-report', lmdController.lmdGateParameterOverviewReport);
 router.get('/lmd-hr-gate-report', lmdController.lmdHrGateReport);
